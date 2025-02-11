7e665601467090134608149e6deb11e2415f6e69
fabien <guigner.f@gmail.com>
2025-02-11 14:28:46 +0100

    [Feat] testHW: add gitignore + initialize project

---
+++ TestHW/TestHW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
